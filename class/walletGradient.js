dc94a859418cb06988e9f2179f4bee9f1c60f325
HashEngineering <hashengineeringsolutions@gmail.com>
2020-05-02 18:37:26 -0700

    Update wallet card colors

--- class/walletGradient.js
+++ class/walletGradient.js
@@ -9,15 +9,15 @@ import { PlaceholderWallet } from './placeholder-wallet';
 import { SegwitBech32Wallet } from './segwit-bech32-wallet';
 
 export default class WalletGradient {
-  static hdSegwitP2SHWallet = ['#65ceef', '#68bbe1'];
-  static hdSegwitBech32Wallet = ['#68bbe1', '#3b73d4'];
+  static hdSegwitP2SHWallet = ['#00A5BD', '#00618F'];
+  static hdSegwitBech32Wallet = ['#00A5BD', '#00618F'];
   static segwitBech32Wallet = ['#f8bbe1', '#945a90'];
   static watchOnlyWallet = ['#7d7d7d', '#4a4a4a'];
   static legacyWallet = ['#40fad1', '#15be98'];
   static hdLegacyP2PKHWallet = ['#e36dfa', '#bd10e0'];
   static hdLegacyBreadWallet = ['#fe6381', '#f99c42'];
   static defaultGradients = ['#c65afb', '#9053fe'];
-  static lightningCustodianWallet = ['#f1be07', '#f79056'];
+  static lightningCustodianWallet = ['#00A5BD', '#00618F'];
   static createWallet = ['#eef0f4', '#eef0f4'];
 
   static gradientsFor(type) {
