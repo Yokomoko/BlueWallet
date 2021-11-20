5c4b7f37f406efd51b5fc3401f67712d4e04cb50
HashEngineering <hashengineeringsolutions@gmail.com>
2021-11-20 07:53:49 -0800

    fix: show address/addresses for inputs and outputs

--- screen/transactions/details.js
+++ screen/transactions/details.js
@@ -89,11 +89,14 @@ const TransactionsDetails = () => {
       if (tx.hash === hash) {
         foundTx = tx;
         for (const input of foundTx.inputs) {
-          from = from.concat(input.addresses);
+          if (input.addresses) from = from.concat(input.addresses);
+          if (input.address) from = from.concat(input.address);
         }
         for (const output of foundTx.outputs) {
           if (output.addresses) to = to.concat(output.addresses);
+          if (output.address) to = to.concat(output.address);
           if (output.scriptPubKey && output.scriptPubKey.addresses) to = to.concat(output.scriptPubKey.addresses);
+          if (output.scriptPubKey && output.scriptPubKey.address) to = to.concat(output.scriptPubKey.address);
         }
       }
     }
